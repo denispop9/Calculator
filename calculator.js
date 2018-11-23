df829d6db476885eedcae14b52ab9f51a72b407d
Denis Pop <denispop10@gmail.com>
2018-11-23 15:39:49 +0000

    progress

--- calculator.js
+++ calculator.js
@@ -1,5 +1,16 @@
 function addNumberToDisplay(number) {
 	var displayScreen = document.getElementById('displayScreen');
-	displayScreenValue = displayScreen.value
+	var displayScreenValue = displayScreen.value
 	displayScreen.value = displayScreenValue + number
+}
+function buttonClickedCalculate(operator) {
+	var displayScreen = document.getElementById('displayScreen');
+	var displayScreenValue = displayScreen.value
+	displayScreen.value = displayScreenValue + operator
+}
+function evaluateEquation() {
+	var displayScreen = document.getElementById('displayScreen');
+	var displayScreenValue = displayScreen.value;
+	var evaluateEquation = eval(displayScreen.value)
+	displayScreen.value = evaluateEquation;
 }
