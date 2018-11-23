b4b1fd84e075a62905c80814c66b9ba7e5aa488a
Denis Pop <denispop10@gmail.com>
2018-11-23 11:12:00 +0000

    multiple numbers on screen

---
+++ calculator.js
@@ -0,0 +1,5 @@
+function addNumberToDisplay(number) {
+	var displayScreen = document.getElementById('displayScreen');
+	displayScreenValue = displayScreen.value
+	displayScreen.value = displayScreenValue + number
+}
